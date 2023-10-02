4fe850e5a6914b70fbb8e4a22007f14ac942ba07
PlutovaKate <114903556+PlutovaKate@users.noreply.github.com>
2023-10-02 18:55:04 +0300

    change title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,22 +5,23 @@ const Statistics = ({ title, stats }) => {
   return (
     <div>
       <section class={css.statistics}>
-        <h2 class={css.title}>Upload stats</h2>
-        <div class={css.statList}>
+        {title && <h2 class={css.title}>Upload stats</h2>}
+
+        <ul class={css.statList}>
           {stats.map(({ id, label, percentage }) => (
             <li class={css.item} key={id}>
               <span class={css.label}>{label}</span>
               <span class={css.percentage}>{percentage}%</span>
             </li>
           ))}
-        </div>
+        </ul>
       </section>
     </div>
   );
 };
 
 Statistics.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
